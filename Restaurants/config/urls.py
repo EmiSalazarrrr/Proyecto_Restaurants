8601f00b80d7e75495cf4273083856461d02a9eb
Gael Cortez <galexandro210@gmail.com>
2026-04-16 20:37:17 -0600

    creacion de menu-cliente, menu-admin, historia-cliente y agregar-ticket

--- Restaurants/config/urls.py
+++ Restaurants/config/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.login_view, name='login'),
     path('registro/', views.registro_view, name='registro'),
+    path('menu-admin/', views.menu_admin_view, name='menu_admin'),
+    path('menu-cliente/', views.menu_cliente_view, name='menu_cliente'),
+    path('agregar-ticket/', views.agregar_ticket_view, name='agregar_ticket'),
+    path('historial/', views.historial_view, name='historial'),
 ]
