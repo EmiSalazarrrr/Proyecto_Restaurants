949efcb1f2577a3476e36bddd800fef8bec1bff2
Gael Cortez <galexandro210@gmail.com>
2026-04-19 20:44:40 -0600

    Pequeño cambio de registro.html, cambios en la urls.py y views.py en la rama Visual

--- Restaurants/config/views.py
+++ Restaurants/config/views.py
@@ -5,3 +5,15 @@ def login_view(request):
 
 def registro_view(request):
     return render(request, 'registro.html')
+
+def menu_admin_view(request):
+    return render(request, 'menu_admin.html')
+
+def menu_cliente_view(request):
+    return render(request, 'menu_cliente.html')
+
+def agregar_ticket_view(request):
+    return render(request, 'agregar_ticket.html')
+
+def historial_view(request):
+    return render(request, 'historial.html')
