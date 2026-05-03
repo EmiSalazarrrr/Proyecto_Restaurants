a252d13333e5dc26675cb0305441198c1b561367
Joel Salazar <emilianosalazar143@gmail.com>
2026-05-03 17:49:31 -0600

    Correción a la vista mobil y algunas animaciones para que haya mejor rendimiento a la hora de entrar a la pagina web.

--- Restaurants/apps/pedidos/migrations/0004_ticket_codigounico_nullable.py
+++ Restaurants/apps/pedidos/migrations/0004_ticket_codigounico_nullable.py
@@ -1,15 +1,26 @@
-from django.db import migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pedidos', '0003_detalleticket_id'),
+        ('pedidos', '0001_initial'),
     ]
 
     operations = [
-        migrations.RunSQL(
-            sql="ALTER TABLE ticket MODIFY COLUMN codigounico INT NULL;",
-            reverse_sql="ALTER TABLE ticket MODIFY COLUMN codigounico INT NOT NULL;",
+        migrations.SeparateDatabaseAndState(
+            database_operations=[
+                migrations.RunSQL(
+                    sql="ALTER TABLE ticket MODIFY COLUMN codigounico INT NULL;",
+                    reverse_sql="ALTER TABLE ticket MODIFY COLUMN codigounico INT NOT NULL;",
+                ),
+            ],
+            state_operations=[
+                migrations.AlterField(
+                    model_name='ticket',
+                    name='codigounico',
+                    field=models.IntegerField(blank=True, null=True, unique=True),
+                ),
+            ],
         ),
     ]
